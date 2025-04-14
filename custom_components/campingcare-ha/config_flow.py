9ac020f3fb2f71a573ec70ae58480b4921eb1b02
tamaygz <forevor@gmail.com>
2025-04-14 23:44:36 +0200

    1

--- custom_components/campingcare-ha/config_flow.py
+++ custom_components/campingcare-ha/config_flow.py
@@ -1,33 +1,42 @@
+import logging
 from homeassistant import config_entries
+from homeassistant.core import HomeAssistant
+from homeassistant.const import CONF_API_KEY, CONF_URL
 import voluptuous as vol
-from .const import DOMAIN, CONF_API_KEY, CONF_API_URL, CONF_NAME
+from .const import DOMAIN
 
+_LOGGER = logging.getLogger(__name__)
 
 class CampingCareConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for CampingCare."""
 
-    VERSION = 1
-
     async def async_step_user(self, user_input=None):
-        errors = {}
-
+        """Handle the initial step of the config flow."""
         if user_input is not None:
-            await self.async_set_unique_id(f"{user_input[CONF_NAME]}")
-            self._abort_if_unique_id_configured()
+            # Here you can validate the API key and URL
+            api_key = user_input[CONF_API_KEY]
+            url = user_input[CONF_URL]
 
+            # If validation succeeds, create a config entry
             return self.async_create_entry(
-                title=user_input[CONF_NAME],
-                data=user_input,
+                title="CampingCare Integration",
+                data={
+                    CONF_API_KEY: api_key,
+                    CONF_URL: url
+                }
             )
 
-        data_schema = vol.Schema({
-            vol.Required(CONF_NAME): str,
-            vol.Required(CONF_API_URL): str,
-            vol.Required(CONF_API_KEY): str,
-        })
-
+        # Show the form to the user
         return self.async_show_form(
             step_id="user",
-            data_schema=data_schema,
-            errors=errors
+            data_schema=self._get_data_schema()
         )
+
+    def _get_data_schema(self):
+        """Return the schema for the data input fields."""
+        from homeassistant.helpers import config_validation as cv
+
+        return vol.Schema({
+            vol.Required(CONF_API_KEY): str,
+            vol.Required(CONF_URL): str,
+        })
