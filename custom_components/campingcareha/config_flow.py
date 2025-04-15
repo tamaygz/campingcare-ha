c7d8940caed64c4825a8e0a66e776c8f33fb13e8
tamaygz <forevor@gmail.com>
2025-04-15 22:17:16 +0200

    optionsflow

--- custom_components/campingcareha/config_flow.py
+++ custom_components/campingcareha/config_flow.py
@@ -48,14 +48,14 @@ class CampingCareConfigFlow(ConfigFlow, domain=DOMAIN):
 class CampingCareOptionsFlowHandler(OptionsFlow):
     """Handle CampingCareHA options."""
 
-    def __init__(self, config_entry: ConfigEntry) -> None:
-        """Initialize options flow."""
-        self.config_entry = config_entry
+    # def __init__(self, config_entry: ConfigEntry) -> None:
+    #     """Initialize options flow."""
+    #     self.config_entry = config_entry
 
     async def async_step_init(self, user_input=None) -> ConfigFlowResult:
         """Manage the CampingCareHA options."""
         if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
+            return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
 
         return self.async_show_form(
             step_id="init",
